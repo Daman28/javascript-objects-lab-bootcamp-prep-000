1e50970d5837e785df5ddbf250fafbb6d1b03514
Damanjeet Singh <damanjeetmar28@gmail.com>
2018-06-29 03:14:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var recipes = {eggs: 3, flour: '2 cups', chocolate: '3 cup'};
 
 function updateObjectWithKeyAndValue(obj, key, value) {
-  return Object.assign({},)
+  return Object.assign({},obj,)
 }
